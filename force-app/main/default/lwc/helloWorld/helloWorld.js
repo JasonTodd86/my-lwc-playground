6910f889c389a43d44408bc82e0d8ca082421fe8
Jason Todd <j.chris.todd@gmail.com>
2026-09-23 06:49:55 -0500

    conditional rendering and lists

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-    greeting = 'World';
+    greeting = '';
+    history = [];
 
     handleChange(event) { // handleChange receives an event object automatically — every DOM event handler in LWC does.
         this.greeting = event.target.value;
@@ -9,4 +10,13 @@ export default class HelloWorld extends LightningElement {
     // event.target is the DOM element that fired the event (the input box), 
     // and event.target.value is whatever's currently typed in it. 
     // We assign that into this.greeting, which — because it's reactive — instantly updates the {greeting} text below it.
+    handleSave(){
+        if(this.greeting){
+            this.history = [...this.history, this.greeting];
+        }
+    }
+
+    get hasGreeting(){
+        return this.greeting.length > 0;
+    }
 }
